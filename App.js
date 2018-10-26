494aea94de52294af250771b16aa4988a5fa4518
kassanj <kassandra@whereby.us>
2018-10-25 17:15:41 -0700

    feat: set icon helper function and render icon

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AddEntry from './components/AddEntry'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <AddEntry />
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
