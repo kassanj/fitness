00c84d451f8da6ef63b5b0a0cae5fe5f3b3ddde0
kassanj <kassandra@whereby.us>
2018-10-26 14:50:38 -0700

    feat: add AppStatusBar to style very top of screen

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Platform } from 'react-native';
+import { View, Platform, StatusBar } from 'react-native';
 import AddEntry from './components/AddEntry';
 import History from './components/History';
 import { createStore } from 'redux';
@@ -8,6 +8,16 @@ import reducer from './reducers';
 import { createBottomTabNavigator } from 'react-navigation'
 import { purple, white } from './utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { Constants } from 'expo'
+
+
+ function AppStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
 
 const Tabs = createBottomTabNavigator({
   History: {
@@ -49,6 +59,7 @@ export default class App extends React.Component {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
+          <AppStatusBar backgroundColor={purple} barStyle="light-content" />
           <Tabs />
         </View>
       </Provider>
